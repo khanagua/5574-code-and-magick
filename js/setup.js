baa0be735a2f1aa1c0abe2c256b67ab0394fe3a8
Melamara <v.khanagua@ya.ru>
2017-04-08 17:47:02 +0300

    edit module3-task1

--- js/setup.js
+++ js/setup.js
@@ -3,13 +3,6 @@
 // Показываем окно настроек
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
-getRandomObject();
-
-// Массивы констант
-var WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SONAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COLOR_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_COLOR_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 
 /**
 * Получить рандомное целое число
@@ -17,28 +10,83 @@ var WIZARD_COLOR_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 * @param {Number} max максимально число
 * @return {Number}
 */
-
-function() {
-  for (var i = 0; i <= 8; i++) {
-  var wizards = [];
+function getRandomNumber(min, max) {
+  var randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  return randomNumber;
 }
 
-
 /**
-* Получить объект из рандомных данных
+* Создать объект из рандомных данных
 * @return {object}
 */
-function getRandomObject() {
+function makeRandomObject() {
   var min = 0;
-  var RandomNumberName = Math.random() * (WIZARD_NAME.length - min) + min;
-  var RandomNumberSoname = Math.random() * (WIZARD_SONAME.length - min) + min;
-  var RandomNumberCoat = Math.random() * (WIZARD_COLOR_COAT.length - min) + min;
-  var RandomNumberEyes = Math.random() * (WIZARD_COLOR_EYES.length - min) + min;
+  var WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var WIZARD_SONAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var WIZARD_COLOR_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var WIZARD_COLOR_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+  var randomNumberName = getRandomNumber(min, WIZARD_NAME.length);
+  var randomNumberSoname = getRandomNumber(min, WIZARD_SONAME.length);
+  var randomNumberCoat = getRandomNumber(min, WIZARD_COLOR_COAT.length);
+  var randomNumberEyes = getRandomNumber(min, WIZARD_COLOR_EYES.length);
   var randomObject = {
-    name: WIZARD_NAME[RandomNumberName] + ' ' + WIZARD_SONAME[RandomNumberSoname],
-    coatColor: WIZARD_COLOR_COAT[RandomNumberCoat],
-    eyesColor: WIZARD_COLOR_EYES[RandomNumberEyes],
+    name: WIZARD_NAME[randomNumberName] + '<br>' + WIZARD_SONAME[randomNumberSoname],
+    // пришлось добавить <br>, иначе сильно разметка съезжает
+    // и иногда слипаются маги -)
+    coatColor: WIZARD_COLOR_COAT[randomNumberCoat],
+    eyesColor: WIZARD_COLOR_EYES[randomNumberEyes],
   };
   return randomObject;
 }
+
+var numberWizards = 4;  // количество магов
+/**
+* Создать массив магов
+* @return {object}
+*/
+function makeArr() {
+  var wizardsArr = [];
+  for (var i = 0; i < numberWizards; i++) {
+    wizardsArr[i] = makeRandomObject();
+  }
+  return wizardsArr;
+}
+
+// создаем массив магов
+var wizards = makeArr();
+
+// отрисовываем элементы
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+/**
+* Заполнить шаблон мага
+* @param {object} wizard элемент массива магов
+* @return {DOM-object}
+*/
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').innerHTML = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+/**
+* Подготовить фрагмент с элементами для вставки
+* @return {DOM-object}
+*/
+var renderFragment = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < numberWizards; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  return fragment;
+};
+
+// Добавляем магов на страницу
+similarListElement.appendChild(renderFragment());
+
+// Показываем окно похожих персонажей
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
